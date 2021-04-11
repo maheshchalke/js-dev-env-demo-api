908b50fd58cd7403cf30565e73660e76c80624b8
Mahesh Chalke <maheshchalke@gmail.com>
2021-04-11 20:56:20 +0530

    package-lock.json

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.get('/', function(request, response) {
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    {"id": 1, "firstName": "Mahesh", "lastName": "Chalke", "email": "maheshchalke@gmail.com"},
+    {"id": 2, "firstName": "Ram", "lastName": "Patil", "email": "patilram3@gmail.com"},
+    {"id": 3, "firstName": "Prateek", "lastName": "Dushing", "email": "prateekdushing@gmail.com"}
   ]);
 });
 
